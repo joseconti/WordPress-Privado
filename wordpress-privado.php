4721edebacd67c61c8ca5f7584d7577420379516
José Conti <j.conti@joseconti.com>
2013-01-27 16:23:54 +0100

    Cambio de texto

--- wordpress-privado.php
+++ wordpress-privado.php
@@ -1,4 +1,4 @@
-<?php //Esta simple funbción hace tu WordPress totalmente privado
+<?php //Esta simple función hace tu WordPress totalmente privado. Solo es accesible wp-login.php y wp-register.php
 
 function is_login_page_contapress() {
     return in_array($GLOBALS['pagenow'], array('wp-login.php', 'wp-register.php'));
